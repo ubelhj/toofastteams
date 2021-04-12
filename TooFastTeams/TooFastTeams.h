c20295905a04d9bc7a3f3b3aa3d4f9d39c800307
ubelhj <ubelhj@uw.edu>
2021-04-12 11:59:51 -0700

    adds NetcodeManager

--- TooFastTeams/TooFastTeams.h
+++ TooFastTeams/TooFastTeams.h
@@ -2,6 +2,7 @@
 
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
+#include "../NetcodeManager/NetcodeManager.h"
 
 #include "version.h"
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
